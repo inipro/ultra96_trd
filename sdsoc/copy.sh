9ae31b749e564895e92404f2da718aa8a654f8cf
Hyunok Kim <hokim@inipro.net>
2018-12-20 01:52:15 +0900

    Fix sdsoc copy

--- sdsoc/copy.sh
+++ sdsoc/copy.sh
@@ -2,6 +2,7 @@ cp  ../petalinux/ultra96_trd/images/linux/zynqmp_fsbl.elf resources/a53_linux/bo
 cp  ../petalinux/ultra96_trd/images/linux/u-boot.elf resources/a53_linux/boot/
 cp  ../petalinux/ultra96_trd/images/linux/bl31.elf resources/a53_linux/boot/
 cp  ../petalinux/ultra96_trd/images/linux/pmufw.elf resources/a53_linux/boot/
+mkdir resources/a53_linux/image/
 cp  ../petalinux/ultra96_trd/images/linux/image.ub resources/a53_linux/image/
 
 cp  ../petalinux/ultra96_trd/project-spec/hw-description/system.hdf resources/prebuilt/ultra96_trd.hdf
